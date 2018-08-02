7839ee022d77e35f432b2e5eb1d75fc5805bf382
sangre5 <pjuradosolis@gmail.com>
2018-08-02 10:20:28 +0200

    Definicion de personajes

---
+++ Zombirama/Personaje.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zombirama
+{
+    class Personaje
+    {
+        public int PersonajeID { get; set; }
+        public string Nombre { get; set; }    
+        public string Comentarios {get; set;}
+
+        public enum EstatusE { Vivo, Muerto };
+        public enum RelevanciaE { Principal, Secundario, Incidental };
+
+        private EstatusE _estatus;
+
+        public EstatusE Estatus
+        {
+            get
+            {
+                return this._estatus;
+            }
+            set
+            {
+                this._estatus = value;
+            }
+            
+        }
+
+        private RelevanciaE _relevancia;
+
+        public RelevanciaE Relevancia
+        {
+            get
+            {
+                return this._relevancia;
+            }
+            set
+            {
+                this._relevancia = value;
+            }
+        }
+
+        public Personaje (string nombre, int edad, RelevanciaE relevancia)
+        {
+            this.Nombre = nombre;
+            this.Relevancia = relevancia;
+            this.Estatus = EstatusE.Vivo;
+        }
+
+
+    }
+}
